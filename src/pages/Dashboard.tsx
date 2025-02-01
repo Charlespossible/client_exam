ff7ebf047fa7a9b1330dc9e9318d8a1a26bc9025
Charles <Cosmasndu@gmail.com>
2025-02-02 00:07:14 +0100

    (dashboard bug fixed)

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-//import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { FaChartLine, FaTrophy, FaClipboardList } from "react-icons/fa";
 import Sidebar from "../components/Sidebar";
 import StatsCard from "../components/StatsCard";
-//import Profile from "../components/profile";
+
 
 const Dashboard: React.FC = () => {
   return (
